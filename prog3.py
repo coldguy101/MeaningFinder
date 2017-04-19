34f78986398bb9f801850960dd2fcc6082af4e54
Santiago Andaluz <Santiago_andaluz_ruiz@baylor.edu>
2017-04-19 17:13:11 -0500

    Done enough

--- prog3.py
+++ prog3.py
@@ -19,7 +19,6 @@ def prepare_text( text ):
     stemmer = SnowballStemmer('english')
 
     text_copy = word_tokenize(text_copy)
-#    text_copy = [stemmer.stem(word) if word[0] > "Z" or word[0] < "A" else word for word in text_copy ]
     text_copy = nltk.pos_tag(text_copy)
 
     return text_copy
@@ -28,7 +27,9 @@ def process( sub, subtree ) :
     lemmatizer = WordNetLemmatizer()
     label = subtree.label();
     sub = lemmatizer.lemmatize(sub.lower(), wordnet.NOUN)
-    #print sub
+    firstNoun = ''
+    secondNoun =  ''
+    action =  ''
     if label == 'TY31':
         #Checks if the chunk is important
         np = subtree[0].leaves()
@@ -42,6 +43,7 @@ def process( sub, subtree ) :
                         if adj[1].startswith('J'):
                             print '3 ' + lemmatizer.lemmatize(adj[0], get_wordnet_pos( adj[1] ) )
                     break
+        return
     elif label == 'TY32':
         #Checks if the chunk is important
         np = subtree[1].leaves()
@@ -53,8 +55,89 @@ def process( sub, subtree ) :
                     part = part.leaves()
                     for adj in part:
                         if adj[1].startswith('J'):
-                            print '3 ' + lemmatizer.lemmatize(adj[0], get_wordnet_pos( adj[1] ) )
+                            print '3 ' + lemmatizer.lemmatize(adj[0].lower(), get_wordnet_pos( adj[1] ) )
                     break
+        return
+    elif label == 'TY11':
+        #NP NP VP
+
+        np = subtree[1].leaves()
+        for word in np:
+            if word[1].startswith('NN'):
+                firstNoun = lemmatizer.lemmatize(word[0].lower(), wordnet.NOUN)
+
+        np = subtree[0].leaves()
+        for word in np:
+            if word[1].startswith('NN'):
+                secondNoun = lemmatizer.lemmatize(word[0].lower(), wordnet.NOUN)
+
+        vp = subtree[2].leaves()
+        for word in vp:
+            if word[1].startswith('VB'):
+                #action = lemmatizer.lemmatize(word[0].lower())
+                action = word[0].lower()
+    elif label == 'TY12':
+        #NP VP NP
+        np = subtree[0].leaves()
+        for word in np:
+            if word[1].startswith('NN'):
+                firstNoun = lemmatizer.lemmatize(word[0].lower(), wordnet.NOUN)
+
+        np = subtree[2].leaves()
+        for word in np:
+            if word[1].startswith('NN'):
+                secondNoun = lemmatizer.lemmatize(word[0].lower(), wordnet.NOUN)
+
+        vp = subtree[1].leaves()
+        for word in vp:
+            if word[1].startswith('VB'):
+                #action = lemmatizer.lemmatize(word[0].lower())
+                action = word[0].lower()
+
+    elif label == 'TY13':
+        # NP VP IN NP
+        np = subtree[0].leaves()
+        for word in np:
+            if word[1].startswith('NN'):
+                firstNoun = lemmatizer.lemmatize(word[0].lower(), wordnet.NOUN)
+
+        np = subtree[3].leaves()
+        for word in np:
+            if word[1].startswith('NN'):
+                secondNoun = lemmatizer.lemmatize(word[0].lower(), wordnet.NOUN)
+
+        vp = subtree[1].leaves()
+        for word in vp:
+            if word[1].startswith('VB'):
+                #action = lemmatizer.lemmatize(word[0].lower())
+                action = word[0].lower()
+
+    elif label == 'TY15':
+        # NP VP
+        np = subtree[0].leaves()
+        for word in np:
+            if word[1].startswith('NN'):
+                firstNoun = lemmatizer.lemmatize(word[0].lower(), wordnet.NOUN)
+
+        secondNoun = None
+
+        vp = subtree[1].leaves()
+        for word in vp:
+            if word[1].startswith('VB'):
+                #action = lemmatizer.lemmatize(word[0].lower())
+                action = word[0].lower()
+
+
+    if secondNoun is None:
+        print '1 ' + action
+    elif firstNoun == sub:
+        #1 action secondnoun
+        print '1 ' + action + ' ' + secondNoun
+    elif secondNoun == sub:
+        #2 action firstnoun
+        print '2 ' + action + ' ' + firstNoun
+    return
+
 
 def get_wordnet_pos( tag ):
     if tag.startswith('J') :
@@ -69,15 +152,14 @@ def get_wordnet_pos( tag ):
         return ''
 
 
-#(name, text) = read_file(raw_input('What is the name for the file to read\n'))
 
 (name, text) = read_file(sys.argv[1])
 
 text_proc = prepare_text(text)
 
-#//pronoun resolution algo
+#pronoun resolution algo
 
-sub_PR = ['he', 'she', 'me', 'my', 'i', 'him', 'her', 'himself', 'herself', 'myself']
+sub_PR = ['he', 'she', 'me', 'my', 'i', 'him', 'her', 'himself', 'herself', 'myself', 'themselves']
 
 counter = 0
 recent_NNP = recent_NN = None
@@ -100,7 +182,7 @@ for word in text_proc:
         recent_NNP = recent_NN = None
     counter += 1
 
-#//actual prog
+#actual prog
 
 grammar = r"""
         VP: {<W.*>*<RB.*>*<VB.*>+<RB.*>*}
@@ -110,7 +192,6 @@ grammar = r"""
         TY11: {<NP><NP><VP>}
         TY12: {<NP><VP><NP>}
         TY13: {<NP><VP><IN><NP>}
-        TY14: {<TY1.*><VP><NP>}
         TY15: {<NP><VP>}
         TY32: {<JJP><NP>}
        """
@@ -121,29 +202,4 @@ result = cp.parse(text_proc)
 for subtree in result.subtrees():
     if subtree.label() != 'VP' and subtree.label() != 'NP' and subtree.label() != 'JJP' and subtree.label() != 'S':
         process(name, subtree)
-        #print subtree
 
-"""
-print text_proc
-for word in text_proc:
-    print word[0] ,
-"""
-
-
-"""
-for sent in brown.tagged_sents():
-    result = cp.parse(sent)
-    for subtree in result.subtrees():
-        if subtree.label() == 'JUNK' : print (subtree)
-"""
-
-
-"""
-for word in text_proc:
-    print word[0] ,
-
-example = "Sam was sitting on the couch. He is watching television. He is happy. His mom walked in the door with Hally, their dog. Hally walked right over to Sam and licked him hello."
-
-withStopwordsRemoved = [word for word in nltk.word_tokenize(example) if word not in nltk.corpus.stopwords.words('english')]
-print withStopwordsRemoved
-"""
